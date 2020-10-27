f2170b2c8e1af3afede0316406902f60f5331669
Brian <brian.holland.pr@gmail.com>
2020-10-26 23:23:49 -0700

    added fun 404

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import { Container } from 'react-bootstrap';
 
 export default function Custom404() {
+	const router = useRouter();
+
+	const nameOfPage = router.asPath.slice(1);
+
 	return (
-		<section className="mt-4">
-			<div style={{ paddingTop: '20%' }}>
-				<h3 className="text-center">
-					<span className="right-wall">404</span> Page Not Found
-				</h3>
-			</div>
-		</section>
+		<Container fluid>
+			<section className="space">
+				<div className="sentence">
+					<h4 className="text-center mb-5">{nameOfPage} was The Imposter</h4>
+				</div>
+				<div className="move">
+					<h2 className="spin">404</h2>
+				</div>
+			</section>
+		</Container>
 	);
 }
