cd25e60f70cf0fc8de3bfdf74300feeb274230c4
Brian <brian.holland.pr@gmail.com>
2020-10-27 01:08:56 -0700

    best fucking 404 ever

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Container } from 'react-bootstrap';
+import Typewriter from 'typewriter-effect';
 
 export default function Custom404() {
 	const router = useRouter();
@@ -9,12 +10,29 @@ export default function Custom404() {
 
 	return (
 		<Container fluid>
-			<section className="space">
-				<div className="sentence">
-					<h4 className="text-center mb-5">{nameOfPage} was The Imposter</h4>
-				</div>
-				<div className="move">
-					<h2 className="spin">404</h2>
+			<section>
+				<div class="stars" />
+				<div class="twinkling" />
+
+				<div style={{ position: 'relative' }}>
+					<h4 className="text-center sentence">
+						<Typewriter
+							onInit={(typewriter) => {
+								console.log(typewriter);
+								typewriter
+									.callFunction(() => {
+										console.log('String typed out!');
+									})
+									.pauseFor(2300)
+									.typeString(`${nameOfPage} was The Imposter`)
+									.start();
+							}}
+						/>
+					</h4>
+
+					<div className="move">
+						<h2 className="spin">404</h2>
+					</div>
 				</div>
 			</section>
 		</Container>
