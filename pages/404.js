f46768abbe5153df97524b08c6b694ca8bddddf3
Brian <brian.holland.pr@gmail.com>
2020-10-26 20:23:19 -0700

    custom 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Custom404() {
+	return (
+		<section className="mt-4">
+			<div style={{ paddingTop: '20%' }}>
+				<h3 className="text-center">404 - Page Not Found</h3>
+			</div>
+		</section>
+	);
+}
