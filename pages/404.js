706cf20f9dfec2c3ff20f9f151925837b3a45411
Brian <brian.holland.pr@gmail.com>
2020-10-26 20:27:20 -0700

    404 line

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,9 @@ export default function Custom404() {
 	return (
 		<section className="mt-4">
 			<div style={{ paddingTop: '20%' }}>
-				<h3 className="text-center">404 - Page Not Found</h3>
+				<h3 className="text-center">
+					<span className="right-wall">404</span> Page Not Found
+				</h3>
 			</div>
 		</section>
 	);
