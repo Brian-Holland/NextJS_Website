87b2ba82b57bb87333581e6cb2869d7fa13d7f88
Brian <brian.holland.pr@gmail.com>
2020-10-27 02:46:46 -0700

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
 import '../styles/bootstrap.min.css';
 import '../styles/styles.css';
-
 import NavBar from '../components/NavBar';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+	const router = useRouter();
+	useEffect(
+		() => {
+			const handleRouteChange = (url) => {
+				gtag.pageview(url);
+			};
+			router.events.on('routeChangeComplete', handleRouteChange);
+			return () => {
+				router.events.off('routeChangeComplete', handleRouteChange);
+			};
+		},
+		[ router.events ]
+	);
+
 	return (
 		<div>
 			<NavBar />
 			<Component {...pageProps} />
 		</div>
 	);
-}
+};
+
+export default App;
