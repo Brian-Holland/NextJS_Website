1bc24db7c38644d4f65e7c14080f007fd0cce3b1
Brian <brian.holland.pr@gmail.com>
2020-11-07 13:46:09 -0800

    arbys...

---
+++ pages/arbys.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const ArbysPage = () => {
+	return (
+		<Container className="text-center" fluid>
+			<img className="fs-img" src="/imgs/2for5.jpg" alt="arbys menu" />
+		</Container>
+	);
+};
+
+export default ArbysPage;
