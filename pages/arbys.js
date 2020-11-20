28e8e51c8a7d5cad88e84be04064d8dba6ee3ece
Brian <brian.holland.pr@gmail.com>
2020-11-19 23:21:22 -0800

    arbys

--- pages/arbys.js
+++ pages/arbys.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Head from 'next/head';
 import { Container } from 'react-bootstrap';
 
 const ArbysPage = () => {
 	return (
-		<Container className="text-center" fluid>
-			<img className="fs-img" src="/imgs/2for5.jpg" alt="arbys menu" />
-		</Container>
+		<div>
+			<Head>
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://www.brian-holland.com" />
+				<meta property="twitter:title" content="Brian Holland | Software Engineer" />
+				<meta
+					property="twitter:description"
+					content="Brian Holland's software engineering portfolio website including a resume, projects, contact info, and custom 404 error page."
+				/>
+				<meta property="twitter:image" content="https://www.brian-holland.com/screenshot.png" />
+				<meta property="twitter:image:alt" content="Screenshot of website homepage" />
+			</Head>
+			<Container className="text-center" fluid>
+				<img className="fs-img" src="/imgs/2for5.jpg" alt="arbys menu" />
+			</Container>
+		</div>
 	);
 };
 
