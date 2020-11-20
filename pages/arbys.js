a92eb034d3d41fbdc5955b24433a84041dcbacc1
Brian <brian.holland.pr@gmail.com>
2020-11-19 23:25:52 -0800

    arbys v3

--- pages/arbys.js
+++ pages/arbys.js
@@ -9,7 +9,12 @@ const ArbysPage = () => {
 				<meta property="twitter:card" content="summary_large_image" />
 				<meta property="twitter:url" content="https://www.brian-holland.com" />
 				<meta property="twitter:title" content="Brian Holland | Software Engineer" />
-				<meta property="twitter:image:alt" content="Arbys" />
+				<meta
+					property="twitter:description"
+					content="Brian Holland's software engineering portfolio website including a resume, projects, contact info, and custom 404 error page."
+				/>
+				<meta property="twitter:image" content="https://www.brian-holland.com/2for5.png" />
+				<meta property="twitter:image:alt" content="Arby's" />
 			</Head>
 			<Container className="text-center" fluid>
 				<img className="fs-img" src="/imgs/2for5.jpg" alt="arbys menu" />
