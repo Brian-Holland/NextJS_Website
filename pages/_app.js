6b60008dc5d8735646782edd60282aed1db481da
Brian <brian.holland.pr@gmail.com>
2021-03-01 18:41:52 -0800

    removed GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,8 @@
 import '../styles/bootstrap.min.css';
 import '../styles/styles.css';
 import NavBar from '../components/NavBar';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
 
 const App = ({ Component, pageProps }) => {
-	const router = useRouter();
-	useEffect(
-		() => {
-			const handleRouteChange = (url) => {
-				gtag.pageview(url);
-			};
-			router.events.on('routeChangeComplete', handleRouteChange);
-			return () => {
-				router.events.off('routeChangeComplete', handleRouteChange);
-			};
-		},
-		[ router.events ]
-	);
-
 	return (
 		<div>
 			<NavBar />
